a3739d7a87ef1dc2bbb8599802491c968068bee0
Jason <jason271277f@gmail.com>
2026-05-02 01:07:18 +0200

    Calculator UPDATE

--- main.java
+++ main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.awt.Frame;
 
 public class Main {
 
@@ -7,15 +6,23 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
+
             double num1, num2;
 
-            System.out.println("Enter Numbers: ");
+            Scanner input = new Scanner(System.in);
 
+            System.out.println("Enter First Number: ");
             num1 = scanner.nextDouble();
+
+            System.out.println("Enter Second Number: ");
             num2 = scanner.nextDouble();
 
             System.out.println("Enter Operator: ");
             char operator = scanner.next().charAt(0);
+            if (operator != '+' && operator != '-' && operator != '*' && operator != '/') {
+                System.out.println("Invalid Operator!");
+                continue;
+            }
 
             double result = 0;
             switch (operator) {
@@ -28,6 +35,14 @@ public class Main {
                 case '-':
                     result = num1 - num2;
                     break;
+                case '/':
+                    if(num2 == 0) {
+                        System.out.println("NO DIVISION BY ZERO");
+                        continue;
+                    }
+                    result = num1 / num2;
+                    break;
+
             }
 
             System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
